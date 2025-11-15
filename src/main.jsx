2a07674e95328de991e43abfe04bb1f2823cac03
omar278m <omartammam515@gmail.com>
2025-11-15 21:05:15 +0200

    Import all.min.css for additional styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './all.min.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
