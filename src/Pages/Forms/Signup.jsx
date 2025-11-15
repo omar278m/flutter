0402652cefe04294a9fe1d5d255249e022c5cae1
omar278m <omartammam515@gmail.com>
2025-11-15 21:06:22 +0200

    Change logo format from SVG to PNG

--- src/Pages/Forms/Signup.jsx
+++ src/Pages/Forms/Signup.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo.png";
 import signup from "../../assets/Forms/medical-banner-with-stethoscope.jpg";
 import facebook from "../../assets/Forms/icons8-facebook-48.png";
 import google from "../../assets/Forms/icons8-google-48.png";
